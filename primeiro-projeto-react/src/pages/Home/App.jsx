37a449124442ae5ce2518aa2ca72bc2b20b6f438
Bruno Cantarelli <cantarellibruno2004@gmail.com>
2023-10-28 16:31:12 -0300

    Finalizado

---
+++ primeiro-projeto-react/src/pages/Home/App.jsx
@@ -0,0 +1,36 @@
+import { useRef, useState } from "react";
+import { v4 } from "uuid"; // para gerar chaves aleatorias
+import { AddButton, Container, Products, RemoveButton } from './styles'
+
+function App() {
+  const [produtos, setProdutos] = useState([]);
+  const inputRef = useRef();
+
+  function btnAdd() {
+    setProdutos([{id: v4(), nome: inputRef.current.value,},...produtos,]); // ...produtos é para que todos as coisas ja digitadas permaneçam na tela
+    inputRef.current.value = '' // para limpar o input
+  }
+
+  function removeElement(id) {
+    console.log(id)
+    setProdutos(produtos.filter(produto => produto.id !== id))// pega produto por produto e ve se o produto tem o id que foi clicado, se sim, ele apaga
+  }
+
+  return (
+    <>
+      <Container>
+        <h1>Lista de Compras</h1>
+          <input placeholder="produto..." ref={inputRef} />
+          <AddButton onClick={btnAdd}>Adicionar</AddButton>        
+        {produtos.map((produto) => (
+          <Products key={produto.id}>
+            <p>{produto.nome}</p>
+            <RemoveButton onClick={() => removeElement(produto.id)}>🗑️</RemoveButton>
+          </Products>
+        ))}
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ primeiro-projeto-react/src/pages/Home/styles.jsx
@@ -0,0 +1,64 @@
+import styled from 'styled-components'
+
+export const Container = styled.div `
+    background-color: #b752ff;
+    padding: 30px;
+    border-radius: 20px;
+
+    h1{
+        color: #fff;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+
+    input{
+        height: 35px;
+        border-radius: 5px;
+        border: none;
+        margin-top: 30px;
+        outline: none;
+        padding-left: 10px;
+    }
+`
+
+export const AddButton = styled.button `
+    background-color: #6cf000;
+    color: #000;
+    height: 35px;
+    border: none;
+    border-radius: 5px;
+    margin-left: 20px;
+    padding: 5px 10px;
+    cursor: pointer;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+
+    &:hover{
+        opacity: 0.8;
+    }
+`
+
+export const Products = styled.div `
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 10px;
+    margin-top: 10px;
+    align-items: center;
+    height: 50px;
+    background-color: #fff;
+    border-radius: 5px; 
+
+    p {
+        text-transform: capitalize; // primeiras letras sempre maiusculas
+        font-weight: bold; // negrito
+    }
+`
+
+
+export const RemoveButton = styled.button `
+    cursor: pointer;
+    padding: 3px;
+    border: none;
+    border-radius: 5px;
+    background-color: transparent;
+    font-size: 20px;
+`
